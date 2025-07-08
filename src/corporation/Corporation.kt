7bae11a8315500d63d08c8b3d0607a399d0da7d1
Konstantin <klyschenko@ourgapps.com>
2025-07-08 16:41:42 +0400

    Module Four Lesson Thirteen Step One

--- src/corporation/Corporation.kt
+++ src/corporation/Corporation.kt
@@ -2,11 +2,21 @@ package corporation
 
 
 fun main() {
-    val assistant = WorkersRepository.findAssistant()
-    assistant?.printInfo()
-    val director = WorkersRepository.findDirector()
-    director?.printInfo()
-    if (assistant != null) {
-        director?.takeCoffee(assistant)
-    }
+//    val assistant = WorkersRepository.findAssistant()
+//    assistant?.printInfo()
+//    val director = WorkersRepository.findDirector()
+//    director?.printInfo()
+//    if (assistant != null) {
+//        director?.takeCoffee(assistant)
+//    }
+//    val directorSalary = director?.salary ?: 0
+//    val assistantSalary = assistant?.salary ?: 0
+//    val sum = directorSalary + assistantSalary
+
+    val a = method()
+    println(a.hashCode())
+}
+
+fun method() {
+
 }
