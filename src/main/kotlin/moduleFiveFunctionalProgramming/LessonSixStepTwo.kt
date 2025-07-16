df1b66a3dd5e3e66c7784cebbcd82702ec4ca2a1
Konstantin <klyschenko@ourgapps.com>
2025-07-16 13:49:42 +0400

    Module five lesson six step two

--- src/main/kotlin/moduleFiveFunctionalProgramming/LessonSixStepTwo.kt
+++ src/main/kotlin/moduleFiveFunctionalProgramming/LessonSixStepTwo.kt
@@ -1,12 +1,5 @@
 package org.example.moduleFiveFunctionalProgramming
 
-// TODO: Определить универсальную функцию transform
-// 1. Использовать дженерики `<T, R>` для параметризации типов.
-// 2. Принимать коллекцию элементов типа `T`.
-// 3. Принимать лямбда-выражение, преобразующее элемент типа `T` в элемент типа `R`.
-// 4. Преобразовать каждый элемент коллекции с помощью переданного лямбда-выражения.
-// 5. Вернуть новую коллекцию с элементами типа `R`.
-
 fun <T, R> transform(parameters: List<T>, operation: (T) -> R) : List<R> {
     val result = mutableListOf<R>()
     for (parameter in parameters) {
