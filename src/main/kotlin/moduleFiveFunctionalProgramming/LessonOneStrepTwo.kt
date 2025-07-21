274c365deb954f96634a4f8cdb88eb232b7dddfa
Konstantin <klyschenko@ourgapps.com>
2025-07-21 18:02:54 +0400

    Module five lesson Fifteen step two

--- src/main/kotlin/moduleFiveFunctionalProgramming/LessonOneStrepTwo.kt
+++ src/main/kotlin/moduleFiveFunctionalProgramming/LessonOneStrepTwo.kt
@@ -1,34 +1,34 @@
 package moduleFiveFunctionalProgramming
 
-import kotlinx.serialization.Serializable
-import kotlinx.serialization.json.Json
-
-
-// Класс User, который необходимо сериализовать и десериализовать
-@Serializable
-data class User(
-    val id: Int,
-    val name: String,
-    val email: String,
-    val registeredAt: String
-)
-
-// Реализуйте эту функцию для сериализации объекта User
-fun serializeUser(user: User): String {
-    return Json.encodeToString(user)
-}
-
-// Реализуйте эту функцию для десериализации строки JSON в объект User
-fun deserializeUser(json: String): User {
-//    val deserialized = Json.decodeToString(json)
-    val str = Json.decodeFromString<User>(json)
-//    return User(0, "", "", "")
-    return str
-}
-
-fun main() {
-    val user = User(1, "Hon", "alkawtan@gmail.com", "Long time age")
-    val json = serializeUser(user)
-    val str = deserializeUser(json)
-    print(str)
-}
+//import kotlinx.serialization.Serializable
+//import kotlinx.serialization.json.Json
+//
+//
+//// Класс User, который необходимо сериализовать и десериализовать
+//@Serializable
+//data class User(
+//    val id: Int,
+//    val name: String,
+//    val email: String,
+//    val registeredAt: String
+//)
+//
+//// Реализуйте эту функцию для сериализации объекта User
+//fun serializeUser(user: User): String {
+//    return Json.encodeToString(user)
+//}
+//
+//// Реализуйте эту функцию для десериализации строки JSON в объект User
+//fun deserializeUser(json: String): User {
+////    val deserialized = Json.decodeToString(json)
+//    val str = Json.decodeFromString<User>(json)
+////    return User(0, "", "", "")
+//    return str
+//}
+//
+//fun main() {
+//    val user = User(1, "Hon", "alkawtan@gmail.com", "Long time age")
+//    val json = serializeUser(user)
+//    val str = deserializeUser(json)
+//    print(str)
+//}
